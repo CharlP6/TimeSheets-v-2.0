7cf6daeae93bf8bb8f54b812a436adb7c46ad55a
CharlP6 <pretorius.charl6@gmail.com>
2016-06-01 21:39:01 +0200

    DB Conn

--- TimeSheetsV2_0/TimeSheetsV2_0/Form1.cs
+++ TimeSheetsV2_0/TimeSheetsV2_0/Form1.cs
@@ -12,9 +12,31 @@ namespace TimeSheetsV2_0
 {
     public partial class Form1 : Form
     {
+
+        System.Data.OleDb.OleDbConnection TimeSheetConnection = new System.Data.OleDb.OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=\\MINIPC\Users\Charl\Source\Repos\TimeSheets-v-2.0\Engineering Timesheets.accdb");
+        System.Data.OleDb.OleDbCommand TimeSheetCommand = new System.Data.OleDb.OleDbCommand();
+
+
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TimeSheetCommand.CommandText = @"SELECT * FROM Activities WHERE Activity = 'Test Activity'";
+            TimeSheetCommand.CommandType = CommandType.Text;
+            TimeSheetCommand.Connection = TimeSheetConnection;
+            
+            TimeSheetConnection.Open();
+
+            System.Data.OleDb.OleDbDataReader r = TimeSheetCommand.ExecuteReader();
+            TimeSheetConnection.Close();
+
+            MessageBox.Show(r.ToString());
+        }
+
+
     }
 }
--- TimeSheetsV2_0/TimeSheetsV2_0/Properties/Settings.Designer.cs
+++ TimeSheetsV2_0/TimeSheetsV2_0/Properties/Settings.Designer.cs
@@ -8,22 +8,29 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace TimeSheetsV2_0.Properties
-{
-
-
+namespace TimeSheetsV2_0.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "12.0.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=\"C:\\Users\\Charl\\Source\\Repos\\TimeSh" +
+            "eets-v-2.0\\Engineering Timesheets.accdb\"")]
+        public string Engineering_TimesheetsConnectionString {
+            get {
+                return ((string)(this["Engineering_TimesheetsConnectionString"]));
             }
         }
     }
