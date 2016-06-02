92b284113cf061187bde23157fd26f26d965d2d0
CharlP6 <pretorius.charl6@gmail.com>
2016-06-02 11:33:10 +0200

    aaa

--- TimeSheetsV2_0/TimeSheetsV2_0/Form1.cs
+++ TimeSheetsV2_0/TimeSheetsV2_0/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,29 +13,21 @@ namespace TimeSheetsV2_0
 {
     public partial class Form1 : Form
     {
-
-        System.Data.OleDb.OleDbConnection TimeSheetConnection = new System.Data.OleDb.OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=\\MINIPC\Users\Charl\Source\Repos\TimeSheets-v-2.0\Engineering Timesheets.accdb");
-        System.Data.OleDb.OleDbCommand TimeSheetCommand = new System.Data.OleDb.OleDbCommand();
-
-
-
         public Form1()
         {
             InitializeComponent();
         }
 
+        OleDbConnection TimeSheetConnection;
+
         private void button1_Click(object sender, EventArgs e)
         {
-            TimeSheetCommand.CommandText = @"SELECT * FROM Activities WHERE Activity = 'Test Activity'";
-            TimeSheetCommand.CommandType = CommandType.Text;
-            TimeSheetCommand.Connection = TimeSheetConnection;
-            
-            TimeSheetConnection.Open();
 
-            System.Data.OleDb.OleDbDataReader r = TimeSheetCommand.ExecuteReader();
-            TimeSheetConnection.Close();
+        }
 
-            MessageBox.Show(r.ToString());
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
 
 
