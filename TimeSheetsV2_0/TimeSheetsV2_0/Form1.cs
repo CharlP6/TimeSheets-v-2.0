a0ba0831f1682be6346108e30ddab53f99b03e70
CharlP6 <pretorius.charl6@gmail.com>
2016-06-06 15:23:32 +0200

    ccc

--- TimeSheetsV2_0/TimeSheetsV2_0/Form1.cs
+++ TimeSheetsV2_0/TimeSheetsV2_0/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,21 +14,43 @@ namespace TimeSheetsV2_0
 {
     public partial class Form1 : Form
     {
+        AppSettingsReader ASR;// = new AppSettingsReader();
+
+        string dbConnectionString;
+
+        static string ReadSetting(string key)
+        {
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+                string result = appSettings.AllKeys.ToString();
+                return result;
+            }
+            catch (ConfigurationErrorsException)
+            {
+                return "";
+            }
+        }
+
         public Form1()
         {
             InitializeComponent();
+
         }
+        
+
 
         OleDbConnection TimeSheetConnection;
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+           
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            dbConnectionString = ReadSetting("Database Path");
+            MessageBox.Show(dbConnectionString);
         }
 
 
