ff8736a82313817fe2597ae0e675087dbc168fb2
CharlP6 <pretorius.charl6@gmail.com>
2016-06-24 14:52:16 +0200

    aa

--- TimesheetUserInterface/TimesheetUserInterface/MainTimesheetForm.cs
+++ TimesheetUserInterface/TimesheetUserInterface/MainTimesheetForm.cs
@@ -31,7 +31,7 @@ namespace TimesheetUserInterface
 
         private void LoadDatabase()
         {
-            dba = new TSDataBaseAdapter(@"provider=Microsoft.ACE.OLEDB.12.0; Data Source=E:\\V2\Engineering Timesheets Dev Test.accdb ", Environment.UserName);//\\g5ho-fs02\Public-ENC\Design and Planning\Timesheets\V2\Engineering Timesheets Dev Test.accdb ", Environment.UserName);
+            dba = new TSDataBaseAdapter(@"provider=Microsoft.ACE.OLEDB.12.0; Data Source=\\g5ho-fs02\Public-ENC\Design and Planning\Timesheets\V2\Engineering Timesheets Dev Test.accdb ", Environment.UserName);
             if (dba.UserID == -1)
             {
                 UserProfileForm upf = new UserProfileForm();
